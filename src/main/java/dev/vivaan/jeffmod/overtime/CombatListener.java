d22590badaaa03f56a0a7588560c47d5b8031da7
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-03-26 19:38:00 -0700

    added commands to control game

--- src/main/java/dev/vivaan/jeffmod/overtime/CombatListener.java
+++ src/main/java/dev/vivaan/jeffmod/overtime/CombatListener.java
@@ -1,5 +1,11 @@
 package dev.vivaan.jeffmod.overtime;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class CombatListener implements Listener{
 
@@ -24,5 +30,26 @@ public class CombatListener implements Listener{
             plugin.setTime(victim.getUniqueId(), victimTime - timeToTake);
             plugin.setTime(attacker.getUniqueId(), Math.min(attackerTime + timeToTake, 300));
         }
+    }
+
+    @EventHandler
+    public void onKill(PlayerDeathEvent event) {
+        Player victim = event.getEntity();
+        Player attacker = victim.getKiller();
+
+        if (attacker != null) {
+            int victimTime = plugin.getTime(victim.getUniqueId());
+
+            if (victimTime > 0) {
+                int killerTime = plugin.getTime(attacker.getUniqueId());
+
+                plugin.setTime(attacker.getUniqueId(), Math.min(killerTime + (victimTime / 2), 300));
+
+                attacker.sendMessage(ChatColor.GOLD + "You stole " + (victimTime / 2) + "s from " + victim.getName() + "!");
+                attacker.playSound(attacker.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
+            }
+        }
+
+        victim.sendMessage(ChatColor.RED + "You have been killed. You may watch in spectator mode, until the host runs /reset.");
     }
 }
--- src/main/java/dev/vivaan/jeffmod/overtime/Overtime.java
+++ src/main/java/dev/vivaan/jeffmod/overtime/Overtime.java
@@ -4,8 +4,12 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
@@ -14,15 +18,22 @@ import java.util.UUID;
 public final class Overtime extends JavaPlugin {
 
     private final HashMap<UUID, Integer> playerTime = new HashMap<>();
+    private boolean gameRunning = false;
 
     @Override
     public void onEnable() {
+        getCommand("start").setExecutor(this);
+        getCommand("reset").setExecutor(this);
+
         getServer().getPluginManager().registerEvents(new CombatListener(this), this);
 
         getLogger().info("Overtime running!");
         new BukkitRunnable() {
             @Override
             public void run() {
+                if (!gameRunning) {
+                    return;
+                }
 
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     UUID id = player.getUniqueId();
@@ -41,15 +52,48 @@ public final class Overtime extends JavaPlugin {
         }.runTaskTimer(this, 0L, 20L); // 0 delay, every 20 ticks / 1 second
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.isOp()){
+            return true;
+        }
+
+        if (label.equalsIgnoreCase("start")) {
+            gameRunning = true;
+            Bukkit.broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "THE CLOCK HAS STARTED. YOU HAVE 2 MINUTES TO GATHER RESOURCES BEFORE PVP IS ENABLED.");
+            return true;
+        }
+
+        if (label.equalsIgnoreCase("reset")) {
+            gameRunning = false;
+            playerTime.clear();
+            Bukkit.broadcastMessage(ChatColor.GREEN + "The game has been reset.");
+            return true;
+        }
+
+    return false;
+    }
+
     private void updateTimeInterface(Player player, int seconds) {
-        int mins = seconds / 60;
-        int secs = seconds % 60;
-        String timeFormatted = mins + "m " + secs + "s";
+        int absSeconds = Math.abs(seconds);
+
+        int mins = absSeconds / 60;
+        int secs = absSeconds % 60;
+
+        String prefix;
+
+        if (seconds < 0) {
+            prefix = "-";
+        } else {
+            prefix = "";
+        }
+
+        String timeFormatted = prefix + mins + "m " + secs + "s";
         ChatColor decidedChatColor;
 
         if (mins > 1){
             decidedChatColor = ChatColor.GREEN; // yo i do not care if this is deprecated
-        } else {                                          // literally no tutorial uses anything else
+        } else {                                // literally no tutorial uses anything else
             decidedChatColor = ChatColor.RED;
         }
 
