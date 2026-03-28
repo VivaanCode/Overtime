72bc19443f73ee6c7cb1080e3c726e4454bd6383
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-03-27 20:21:36 -0700

    final cleanup

--- src/main/java/dev/vivaan/jeffmod/overtime/Overtime.java
+++ src/main/java/dev/vivaan/jeffmod/overtime/Overtime.java
@@ -2,15 +2,16 @@ package dev.vivaan.jeffmod.overtime;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
-import org.bukkit.GameMode;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -22,7 +23,7 @@ import org.bukkit.scoreboard.ScoreboardManager;
 
 import java.util.*;
 
-public final class Overtime extends JavaPlugin {
+public final class Overtime extends JavaPlugin implements Listener {
 
     private final HashMap<UUID, Integer> playerTime = new HashMap<>();
     private final HashSet<UUID> playersInGame = new HashSet<>();
@@ -43,6 +44,7 @@ public final class Overtime extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new CombatListener(this), this);
         getServer().getPluginManager().registerEvents(new PotionListener(this), this);
+        getServer().getPluginManager().registerEvents(this, this);
         //getServer().getPluginManager().registerEvents(new GameListener(this), this);
 
         getLogger().info("Overtime running!");
@@ -104,16 +106,17 @@ public final class Overtime extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.isOp()){
-            return true;
-        }
+        org.bukkit.World world = Bukkit.getWorlds().get(0);
+        org.bukkit.WorldBorder border = world.getWorldBorder();
+
+        if (!sender.isOp()) return true;
 
         if (label.equalsIgnoreCase("start")) {
             long count = Bukkit.getOnlinePlayers().stream()
-                    .filter(p -> p.getGameMode() == GameMode.SURVIVAL).count();
+                    .filter(p -> p.getGameMode() != GameMode.SPECTATOR).count();
 
             if (count < 2) {
-                sender.sendMessage(ChatColor.RED + "You need at least 2 players in Survival to start.");
+                sender.sendMessage(ChatColor.RED + "You need at least 2 players not in Spectator to start.");
                 return true;
             }
 
@@ -121,28 +124,37 @@ public final class Overtime extends JavaPlugin {
             playersInGame.clear();
             playerTime.clear();
 
+            border.setCenter(206.0, 106.0);
+            border.setSize(125);
+            border.setSize(3, 900);
+
+            org.bukkit.Location spawnLoc = new org.bukkit.Location(world, 206, 79, 106);
             for (Player player : Bukkit.getOnlinePlayers()) {
-                if (player.getGameMode() == GameMode.SURVIVAL) {
+                if (player.getGameMode() != GameMode.SPECTATOR) {
+                    player.setGameMode(GameMode.SURVIVAL);
                     playersInGame.add(player.getUniqueId());
                     playerTime.put(player.getUniqueId(), 300);
+                    player.teleport(spawnLoc);
                 }
             }
 
-            Bukkit.broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "THE CLOCK HAS STARTED. YOU HAVE 2 MINUTES TO GATHER RESOURCES BEFORE PVP IS ENABLED.");
-            Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.ITALIC + "If you leave the server, you will be removed from the game.");
-            Bukkit.broadcastMessage(ChatColor.GREEN + "How to play: Deal damage and kill players to gain more time. Whoever lives the longest wins.");
+            Bukkit.broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "THE CLOCK HAS STARTED. 15 MINUTE BORDER SHRINK ACTIVE.");
+            Bukkit.broadcastMessage(ChatColor.GREEN + "Deal damage to gain time. Last survivor wins!");
             return true;
         }
 
         if (label.equalsIgnoreCase("reset")) {
             gameRunning = false;
-            playerTime.clear();
             playersInGame.clear();
+            playerTime.clear();
+
+            border.setSize(125);
+
             for (Player player : Bukkit.getOnlinePlayers()) {
                 player.setGlowing(false);
             }
 
-            Bukkit.broadcastMessage(ChatColor.GREEN + "The game has been reset.");
+            Bukkit.broadcastMessage(ChatColor.GREEN + "The game has been reset and border cleared.");
             return true;
         }
 
@@ -166,11 +178,16 @@ public final class Overtime extends JavaPlugin {
             return true;
         }
 
-    return false;
+        return false;
     }
 
     private void updateLeaderboard() {
         List<UUID> players = new ArrayList<>(playersInGame);
+
+        for (UUID uuid : playersInGame) {
+            Player p = Bukkit.getPlayer(uuid);
+            if (p != null) p.setGlowing(false);
+        }
 
         players.sort((a, b) -> playerTime.getOrDefault(b, 0) - playerTime.getOrDefault(a, 0));
 
@@ -186,8 +203,6 @@ public final class Overtime extends JavaPlugin {
             }
             if (i == 0) {
                 player.setGlowing(true);
-            } else {
-                player.setGlowing(false);
             }
 
             int time = getTime(id);
@@ -206,8 +221,8 @@ public final class Overtime extends JavaPlugin {
             String timeFormatted = prefix + mins + "m " + secs + "s";
             String name = player.getName();
 
-            objective.getScore(ChatColor.GREEN + name + ": " + ChatColor.YELLOW + timeFormatted).setScore(5 - i);
-            
+            String displayName = (i + 1) + ". " + name + ": ";
+            objective.getScore(ChatColor.GREEN + displayName + ChatColor.YELLOW + timeFormatted).setScore(5 - i);
         }
         for (Player player : Bukkit.getOnlinePlayers()) {
             player.setScoreboard(scoreboard);
@@ -263,6 +278,37 @@ public final class Overtime extends JavaPlugin {
     public boolean isGameRunning() {
         return gameRunning;
     }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (!gameRunning && !event.getPlayer().isOp()) {
+            event.getPlayer().setGameMode(GameMode.ADVENTURE);
+        } else if (gameRunning && !event.getPlayer().isOp()) {
+            event.getPlayer().setGameMode(GameMode.SPECTATOR);
+        }
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        World world = Bukkit.getWorlds().get(0);
+
+        if (!gameRunning && !event.getPlayer().isOp()) {
+            event.setCancelled(true);
+            event.getPlayer().teleport(new Location(world, 206, 79, 106));
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        World world = Bukkit.getWorlds().get(0);
+
+        if (!gameRunning && !event.getPlayer().isOp()) {
+            event.setCancelled(true);
+            event.getPlayer().teleport(new Location(world, 206, 79, 106));
+        }
+    }
+
+
 
     @Override
     public void onDisable() {
